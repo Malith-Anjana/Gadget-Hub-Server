dc7e5cb891a62f2c81257c6be72b5ef4ee69a72e
Malith Anjana <malithanjana97@gmail.com>
2023-07-04 14:11:29 +0530

    implement search function

--- src/controllers/Product.ts
+++ src/controllers/Product.ts
@@ -1,25 +1,33 @@
 import { Request, Response } from "express";
 import Product, { IProduct } from "../models/Product";
+import { isBooleanObject } from "util/types";
 
 const productController = {
   async getAllProducts(req: Request, res: Response) {
     const cat = req.query.category;
-    const sort = req.query.sort
-console.log(sort);
+    const sort = req.query.sort;
+    const search = req.query.search;
 
-    const sortOrder = ((sort==='up')|| (sort ==='high')) ? 'asc': ((sort==='down')|| (sort ==='low')?'desc':'');
-    const sortBy = ((sort==='up')|| (sort=='down')) ? 'rating': 'price';
-console.log(sortOrder);
+    const sortOrder =
+      sort === "up" || sort === "high"
+        ? "asc"
+        : sort === "down" || sort === "low"
+        ? "desc"
+        : "";
+    const sortBy = sort === "up" || sort == "down" ? "rating" : "price";
 
     const sortOptions: any = {};
     let category: any = {};
+    let searchQuery: any = {};
 
-    sort && (sortOptions[sortBy] = sortOrder === 'asc' ? 1 : -1);
-    cat && (category={category: cat && cat})
-    console.log(sortOptions, category)
-    
+    search && (searchQuery = { title: { $regex: search, $options: "i" } });
+
+    sort && (sortOptions[sortBy] = sortOrder === "asc" ? 1 : -1);
+    cat && (category = { category: cat && cat });
+
     let products: IProduct[] = [];
-    if (category || sortOrder) products = await Product.find(category).sort(sortOptions);
+    if (category || sortOrder || search)
+      products = await Product.find({...category, ...searchQuery}).sort(sortOptions);
     else products = await Product.find();
     res.status(200).json({ results: products, count: products.length });
   },
